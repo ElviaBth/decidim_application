0242700c9f2a6f8d667cfe795be90bd060aba421
elviabth <ejbenedith@gmail.com>
2023-04-26 15:32:07 +0200

    added i18n strings, admin contacts_controller created

--- config/initializers/contact_menu.rb
+++ config/initializers/contact_menu.rb
@@ -1,8 +1,19 @@
 Decidim.menu :menu do |menu|
   menu.add_item :contact, # String or symbol to uniquely define the menu
-              "Contact",
-              "/contacts/new",
+              t("menu.contact"),
+              Rails.application.routes.url_helpers.new_contact_path,
                 position: 8
                 #active: # Optional
-              
+end
+
+Decidim.menu :admin_menu do |menu|
+  menu.add_item :contact,
+                I18n.t("menu.contact", scope: "decidim.admin"),
+                Rails.application.routes.url_helpers.contacts_path,
+                icon_name: "envelope-closed",
+                position: 8,
+                active: [%w(
+                  decidim/admin/dashboard
+                  decidim/admin/metrics
+                ), []]
 end
